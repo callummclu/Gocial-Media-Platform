c9f3f4300fdfb8a522527d77af851d51db7f0617
Callum <callummcluskey100@gmail.com>
2022-08-19 21:53:21 +0100

    added like toggle endpoint

--- services/PostService.go
+++ services/PostService.go
@@ -9,8 +9,10 @@ import (
 
 	"github.com/callummclu/Gocial-Media-Platform/auth"
 	"github.com/callummclu/Gocial-Media-Platform/configs"
+	"github.com/callummclu/Gocial-Media-Platform/middleware"
 	"github.com/callummclu/Gocial-Media-Platform/models"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 func GetPostsByUsername(query string) (posts []models.Post, e error) {
@@ -131,7 +133,7 @@ func GetPosts(c *gin.Context) {
 
 	offset = (offset - 1) * limit
 
-	rows, err := db.Query("select username,title,content,created_at,Id from posts where strpos(username, $1) > 0 OR strpos(title, $1) > 0 OR strpos(content, $1) > 0 ORDER BY created_at DESC LIMIT $2 OFFSET $3 ", query, limit, offset)
+	rows, err := db.Query("select username,title,content,created_at,id from posts where strpos(username, $1) > 0 OR strpos(title, $1) > 0 OR strpos(content, $1) > 0 ORDER BY created_at DESC LIMIT $2 OFFSET $3 ", query, limit, offset)
 
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
@@ -289,16 +291,57 @@ func LikePostById(id int64, token string, username string) error {
 	}
 	defer db.Close()
 
-	// check if user has already liked post
+	var liked_posts_ids []string
 
-	// if user already likes
-	// remove them from likes
-	// remove post from their likes
-	// else
-	// add them to likes
-	// add post to their likes
+	get_likes_stmt := "SELECT likes FROM users WHERE username = $1"
 
-	return nil
+	if err := db.QueryRow(get_likes_stmt, username).Scan(pq.Array(&liked_posts_ids)); err != nil {
+		return err
+	}
+
+	var posts_likes []string
+
+	get_posts_likes_stmt := "SELECT likes FROM posts WHERE id = $1"
+
+	if err := db.QueryRow(get_posts_likes_stmt, id).Scan(pq.Array(posts_likes)); err != nil {
+		return err
+	}
+
+	if posts_likes == nil {
+		posts_likes = []string{}
+	}
+
+	if liked_posts_ids == nil {
+		liked_posts_ids = []string{}
+	}
+
+	if middleware.Contains(liked_posts_ids, strconv.FormatInt(id, 10)) {
+		liked_posts_ids = middleware.Remove(liked_posts_ids, strconv.FormatInt(id, 10))
+		posts_likes = middleware.Remove(posts_likes, username)
+
+	} else {
+		liked_posts_ids = append(liked_posts_ids, strconv.FormatInt(id, 10))
+		posts_likes = append(posts_likes, username)
+	}
+
+	save_users_likes := "UPDATE users SET likes = $1 WHERE username = $2"
+	save_posts_likers := "UPDATE posts SET likes = $1 WHERE id = $2"
+
+	_, err = db.Exec(save_users_likes, pq.Array(liked_posts_ids), username)
+
+	if err != nil {
+		fmt.Print("username")
+		return err
+	}
+
+	_, err = db.Exec(save_posts_likers, pq.Array(posts_likes), id)
+
+	if err != nil {
+		fmt.Print("sentUsername")
+		return err
+	}
+
+	return err
 }
 
 func LikePost(c *gin.Context) {
@@ -321,5 +364,5 @@ func LikePost(c *gin.Context) {
 		c.JSON(400, gin.H{"error": "cannot find post"})
 	}
 
-	c.JSON(200, gin.H{"message": "edited post successfully"})
+	c.JSON(200, gin.H{"message": "liked post successfully"})
 }
